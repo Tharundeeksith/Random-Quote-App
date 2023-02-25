971cb79e248607a256d9e3f37348a8110a67df76
Tharundeeksith <tharundeeksith@gmail.com>
2023-02-25 11:29:34 +0530

    Random-Quote-Generator

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Quotes from "./components/Quotes";
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Quotes/>
+    </div>
+  );
+}
+
+export default App;
